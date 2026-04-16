098d7e26ab533a98a8b74fa2a86ddf4302e5fbea
Clawbot <noreply@brightstacklabs.co.uk>
2026-04-16 09:09:06 +0100

    chore: add Meta Pixel (188391209695379)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,7 @@ export default function RootLayout({
           {children}
         </div>
 
+        {/* Google Analytics */}
         <Script async src="https://www.googletagmanager.com/gtag/js?id=G-BHKDZ3FQM8" />
         <Script id="gtag-config">
           {`
@@ -31,6 +32,26 @@ export default function RootLayout({
             gtag('config', 'G-BHKDZ3FQM8');
           `}
         </Script>
+
+        {/* Meta Pixel */}
+        <Script id="meta-pixel-config">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '188391209695379');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img height="1" width="1" style="display:none"
+            src="https://www.facebook.com/tr?id=188391209695379&ev=PageView&noscript=1" />
+        </noscript>
       </body>
     </html>
   );
