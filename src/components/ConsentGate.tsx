06d3988bd96a31184db3fe82f5784cf5899a3f87
Clawbot <clawbot@brightstacklabs.co.uk>
2026-08-18 21:31:39 +0100

    fix: load Meta Pixel fbevents.js for all users, gate init/PageView behind advertising consent

--- src/components/ConsentGate.tsx
+++ src/components/ConsentGate.tsx
@@ -21,27 +21,35 @@ export default function ConsentGate() {
           </Script>
         </>
       )}
+      <Script
+        id="fb-pixel-base"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+          `,
+        }}
+      />
       {consent.advertising && (
-        <>
-          <Script id="fb-pixel-consent">
-            {`
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '1895307837834496');
-              fbq('track', 'PageView');
-            `}
-          </Script>
-          <noscript>
-            <img height="1" width="1" style={{display:'none'}}
-              src="https://www.facebook.com/tr?id=1895307837834496&ev=PageView&noscript=1" />
-          </noscript>
-        </>
+        <Script id="fb-pixel-init" strategy="afterInteractive">
+          {`
+            fbq('init', '1895307837834496');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+      )}
+      {consent.advertising && (
+        <noscript>
+          <img height="1" width="1" style={{display:'none'}}
+            src="https://www.facebook.com/tr?id=1895307837834496&ev=PageView&noscript=1" />
+        </noscript>
       )}
     </>
   );
